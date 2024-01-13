b6b64337d36306c6dd6018a586f9f82a83a07068
James Caldwell <143050671+1jamescaldwell@users.noreply.github.com>
2024-01-12 23:03:21 -0500

    Update ETL.py

--- ETL.py
+++ ETL.py
@@ -9,65 +9,109 @@
 import sys
 import pypyodbc as odbc
 import pandas as pd 
+import numpy as np
 import os
 
-
-videoGames1 = pd.read_csv('Video_Games.csv')
+# Read the CSV files
   # Video_Game.csv from https://www.kaggle.com/datasets/thedevastator/global-video-game-sales-and-reviews
-  # Popular Games data from https://www.kaggle.com/datasets/arnabchaki/popular-video-games-1980-2023
-  # Steam review data from https://www.kaggle.com/datasets/forgemaster/steam-reviews-dataset
+  # PopularGames data from https://www.kaggle.com/datasets/arnabchaki/popular-video-games-1980-2023
+  # Games.csv data from https://www.kaggle.com/datasets/tamber/steam-video-games
+videoGames1 = pd.read_csv('Video_Games.csv')
+videoGames2 = pd.read_csv('PopularGames.csv')
+videoGames3 = pd.read_csv('Games.csv')
+
+# Print information about each dataframe
+print("Information for videoGames1:")
+print("Number of entries:", videoGames1.shape[0])
+print("Number of columns:", videoGames1.shape[1])
+print("Column names:", videoGames1.columns.tolist())
+
+print("\nInformation for videoGames2:")
+print("Number of entries:", videoGames2.shape[0])
+print("Number of columns:", videoGames2.shape[1])
+print("Column names:", videoGames2.columns.tolist())
+
+print("\nInformation for videoGames3:")
+print("Number of entries:", videoGames3.shape[0])
+print("Number of columns:", videoGames3.shape[1])
+print("Column names:", videoGames3.columns.tolist())
+
+#### VideoGamesInfo Table
+# gameInfo SQL Table is:
+    # Title, console (platform), publisher (developer), genre, summary
+    # There will be a merge of data from all 3 data sources for this table
+
+gameTable1 = []
+gameTable2 = []
+gameTable3 = []
+
+gameTable1.extend(videoGames1[['Name', 'Platform', 'Publisher', 'Genre']].values.tolist())
+gameTable1 = [row + [None] for row in gameTable1]
+
+gameTable2.extend(videoGames2[['Title', 'Genres', 'Summary']].values.tolist())
+# Add None to the 2nd and 3rd columns, and shift 2/3 to 4/5
+for row in gameTable2:
+    row.insert(1, None)  # New 2nd column of None
+    row.insert(2, None)  # New 3rd column of None
+    row.insert(1, row.pop(1))  # Move the element from the new 2nd column to the 4th
+    row.insert(2, row.pop(1))  # Move the element from the new 3rd column to the 5th
+
+gameTable3.extend(videoGames3[['GameName', 'Console']].values.tolist())
+gameTable3 = [row + [None] * 3 for row in gameTable3]
+
+gameTable = gameTable1 + gameTable2 + gameTable3
+
+# Replace nan's with None
+gameTable = [[None if pd.isna(entry) else entry for entry in row] for row in gameTable]
+
+# records = gameTable
+
+def SQL_SeverAdd(tableName,records):
+    print('\n\nAdding Data to SQL Server')
+    DRIVER = 'SQL Server'
+    SERVER_NAME = r'caldwell-lt\SQLEXPRESS'
+    DATABASE_NAME = 'VideoGames'
+
+    conn_string = f"""
+        Driver={{{DRIVER}}};
+        Server={SERVER_NAME};
+        Database={DATABASE_NAME};
+        Trust_Connection=yes;
+    """
+
+    try:
+        conn = odbc.connect(conn_string)
+    except Exception as e:
+        print(e)
+        print('Task is terminated')
+        sys.exit()
+    else:
+        cursor = conn.cursor()
 
 
-# records = videoGames1.index.tolist()
-records = videoGames1[['index', 'Platform']].values.tolist()
+    insert_statement = f"""
+        INSERT INTO {tableName}
+        VALUES (?,?,?,?,?)
+    """
 
-# records = [ 
-#     [1,'Wii'],
-#     [2,'PS2']
-# ]
+    try:
+        for record in records:
+            # print(record) #Prints each line that's entered into SQL Sever
+            cursor.execute(insert_statement, record)        
+    except Exception as e:
+        cursor.rollback()
+        print(e.value)
+        print('Transaction rolled back')
+    else:
+        print('Records inserted successfully')
+        cursor.commit()
+        cursor.close()
+    finally:
+        if conn.connected == 1:
+            print('Connection closed')
+            conn.close()
 
-# Convert each element of the list to a list (each inner list represents a row)
-# records = [[value] for value in records]
-
-DRIVER = 'SQL Server'
-SERVER_NAME = r'caldwell-lt\SQLEXPRESS'
-DATABASE_NAME = 'VideoGames'
-
-conn_string = f"""
-    Driver={{{DRIVER}}};
-    Server={SERVER_NAME};
-    Database={DATABASE_NAME};
-    Trust_Connection=yes;
-"""
-
-try:
-    conn = odbc.connect(conn_string)
-except Exception as e:
-    print(e)
-    print('task is terminated')
-    sys.exit()
-else:
-    cursor = conn.cursor()
-
-
-insert_statement = """
-    INSERT INTO VideoGames
-    VALUES (?,?)
-"""
-
-try:
-    for record in records:
-        print(record)
-        cursor.execute(insert_statement, record)        
-except Exception as e:
-    cursor.rollback()
-    print(e.value)
-    print('transaction rolled back')
-else:
-    print('records inserted successfully')
-    cursor.commit()
-    cursor.close()
-finally:
-    if conn.connected == 1:
-        print('connection closed')
-        conn.close()
+serverAdd = 1
+if serverAdd == 1:
+    tableName = 'VideoGamesInfo'
+    SQL_SeverAdd(tableName,gameTable)
