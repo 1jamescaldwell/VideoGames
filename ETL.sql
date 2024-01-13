c1167cbf87a9e35c0f836fb7ab5be6272509ca0e
James Caldwell <143050671+1jamescaldwell@users.noreply.github.com>
2024-01-12 23:04:37 -0500

    Update ETL.sql

--- ETL.sql
+++ ETL.sql
@@ -41,3 +41,36 @@ SELECT * FROM VideoGames
 WHERE gameIndex < 100;
 
 
+-- Drop VideoGamesInfo
+DROP TABLE VideoGamesInfo;
+
+-- Creating GameInfo
+CREATE TABLE VideoGamesInfo (
+	title VARCHAR(255),
+	console VARCHAR(100) NULL,
+	publisher VARCHAR(255) NULL,
+	genre VARCHAR(255) NULL,
+	summary VARCHAR(5000) NULL,
+)
+
+-- Clear VideoGamesInfo
+DELETE FROM VideoGamesInfo;
+
+-- Select top 1000 from VideoGamesInfo table
+SELECT TOP (1000) [title]
+      ,[console]
+      ,[publisher]
+      ,[genre]
+      ,[summary]
+  FROM [videoGames].[dbo].[VideoGamesInfo]
+
+-- Select 2nd 1000 and order by Title
+SELECT [title]
+      ,[console]
+      ,[publisher]
+      ,[genre]
+      ,[summary]
+FROM [videoGames].[dbo].[VideoGamesInfo]
+ORDER BY [Title] -- Replace [YourOrderByColumn] with the column you want to order by
+OFFSET 1000 ROWS
+FETCH NEXT 1000 ROWS ONLY;
